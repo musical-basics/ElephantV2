c1c6530672551d08abeb0729f7b9f6885f60654c
Lionel Yu <lionel@musicalbasics.com>
2022-11-27 05:43:33 -0800

    Initial Commit

---
+++ ElephantV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElephantV2
+//
+//  Created by Lionel Yu on 11/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
